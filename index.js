424b4013517b65b17af8b8cc8037b13f364e5e7e
bycolour2 <ruskir7@gmail.com>
2020-12-22 21:16:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-import express from 'express'
-import puppeteer from 'puppeteer'
+var express = require("express");
+var puppeteer = require("puppeteer");
 
-import createApp from './app.js'
+const app = express();
 
-const app = createApp(express, puppeteer)
+app.listen(process.env.PORT || 3000, () => {
+  const author = "itmo184230";
 
-app.listen(process.env.PORT || 3000)
+  app.use(function (req, res, next) {
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    next();
+  });
+
+  app.get("/login/", (req, res) => {
+    res.send(author);
+  });
+
+  app.get("/test/", async (req, res) => {
+    const { URL } = req.query;
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto(URL);
+    await page.click("#bt");
+    const value = await page.evaluate(async () => {
+      const input = document.getElementById("inp");
+      return input.value;
+    });
+    res.send(value);
+  });
+
+  app.all("*", (req, res) => {
+    res.send(author);
+  });
+
+  return app;
+});
